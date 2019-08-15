e5e36d2d969d1f47a811db932d79a43a6ecb8469
dphale <dphale@pacbell.net>
2019-08-14 17:19:17 -0700

    launch demo site
    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# LabsSVLaunch
---
+++ triviaQuestions.js
@@ -0,0 +1,62 @@
+var triviaQuestions = [
+  {
+    question : "How many farmers grow coffee globally (millions)?",
+    image : "images/parchiment.jpg",
+    imageName :"Coffee parchiment",
+    answer : 10
+  },
+  {
+    question : "In how many countries does TechnoServe have entrepreneurship training programs?",
+    image : "images/smartDuka.jpg",
+    imageName : "Duka Owner",
+    answer : 24
+  },
+  {
+    question : "What is one solution for 'remote sensing?",
+    image : "images/drone.jpg",
+    imageName : "Drone",
+    answer : ["satellite", "drone", "airplane"]
+  },
+  {
+    question : "In how many countries does TechnoServe operate?",
+    image : "images/drone.jpg",
+    imageName : "Drone",
+    answer : ["satellite", "drone", "airplane"]
+  },
+  {
+    question : "How many people did TNS help last year?",
+    image : "images/drone.jpg",
+    imageName : "Drone",
+    answer : ["satellite", "drone", "airplane"]
+  },
+  {
+    question : "What is the color of the sky?",
+    image : "images/drone.jpg",
+    imageName : "Drone",
+    answer : ["satellite", "drone", "airplane"]
+  },
+  {
+    question : "What is my dog's name?",
+    image : "images/drone.jpg",
+    imageName : "Drone",
+    answer : ["satellite", "drone", "airplane"]
+  },
+  {
+    question : "How long have I worked at TNS?",
+    image : "images/drone.jpg",
+    imageName : "Drone",
+    answer : ["satellite", "drone", "airplane"]
+  },
+  {
+    question : "Where is TNS HQ?",
+    image : "images/drone.jpg",
+    imageName : "Drone",
+    answer : ["satellite", "drone", "airplane"]
+  },
+  {
+    question : "How many fellows are in the Bay Area?",
+    image : "images/drone.jpg",
+    imageName : "Drone",
+    answer : ["satellite", "drone", "airplane"]
+  }
+];
