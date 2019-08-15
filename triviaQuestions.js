36e96a081716ba5dc1d0dff12003bfd0a2fa4f15
dphale <dphale@pacbell.net>
2019-08-14 22:48:28 -0700

    initial answer validation code

--- triviaQuestions.js
+++ triviaQuestions.js
@@ -3,60 +3,70 @@ var triviaQuestions = [
     question : "How many farmers grow coffee globally (millions)?",
     image : "images/parchiment.jpg",
     imageName :"Coffee parchiment",
-    answer : ["10","15","20"]
+    answer : ["10","15","20"],
+    correctAnswer : "b"
   },
   {
     question : "In how many countries does TechnoServe have entrepreneurship training programs?",
     image : "images/smartDuka.jpg",
     imageName : "Duka Owner",
-    answer : ["24","36","48"]
+    answer : ["24","36","48"],
+    correctAnswer : "b"
   },
   {
     question : "What is one solution for 'remote sensing?",
     image : "images/drone.jpg",
     imageName : "Drone",
-    answer : ["satellite", "drone", "airplane"]
+    answer : ["satellite", "drone", "airplane"],
+    correctAnswer : "b"
   },
   {
     question : "In how many countries does TechnoServe operate?",
     image : "images/drone.jpg",
     imageName : "Drone",
-    answer : ["satellite", "drone", "airplane"]
+    answer : ["satellite", "drone", "airplane"],
+    correctAnswer : "b"
   },
   {
     question : "How many people did TNS help last year?",
     image : "images/drone.jpg",
     imageName : "Drone",
-    answer : ["satellite", "drone", "airplane"]
+    answer : ["satellite", "drone", "airplane"],
+    correctAnswer : "b"
   },
   {
     question : "What is the color of the sky?",
     image : "images/drone.jpg",
     imageName : "Drone",
-    answer : ["satellite", "drone", "airplane"]
+    answer : ["satellite", "drone", "airplane"],
+    correctAnswer : "b"
   },
   {
     question : "What is my dog's name?",
     image : "images/drone.jpg",
     imageName : "Drone",
-    answer : ["satellite", "drone", "airplane"]
+    answer : ["satellite", "drone", "airplane"],
+    correctAnswer : "b"
   },
   {
     question : "How long have I worked at TNS?",
     image : "images/drone.jpg",
     imageName : "Drone",
-    answer : ["satellite", "drone", "airplane"]
+    answer : ["satellite", "drone", "airplane"],
+    correctAnswer : "b"
   },
   {
     question : "Where is TNS HQ?",
     image : "images/drone.jpg",
     imageName : "Drone",
-    answer : ["satellite", "drone", "airplane"]
+    answer : ["satellite", "drone", "airplane"],
+    correctAnswer : "b"
   },
   {
     question : "How many fellows are in the Bay Area?",
     image : "images/drone.jpg",
     imageName : "Drone",
-    answer : ["satellite", "drone", "airplane"]
+    answer : ["satellite", "drone", "airplane"],
+    correctAnswer : "b"
   }
 ];
